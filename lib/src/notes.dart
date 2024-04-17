a245a97dab8c1668dc5a58db858736bcf9f838a3
yyoannle <yoann.letacq1@gmail.com>
2024-04-17 15:08:42 +0200

    package for app

---
+++ lib/src/notes.dart
@@ -0,0 +1,27 @@
+class Note {
+  int id;
+  String title;
+  String body;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      body: map['body'],
+    );
+  }
+}
---
+++ lib/src/package/database.dart
@@ -0,0 +1,57 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import '../notes.dart';
+
+class DataManager {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+
+    _database = await initDB();
+    return _database!;
+  }
+
+  Future<Database> initDB() async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'notes.db');
+    return openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute('''
+        CREATE TABLE notes(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          content TEXT
+        )
+      ''');
+    });
+  }
+
+  //CRUD
+
+  Future<List<Note>> getAllNotes() async {
+    final db = await database;
+    var res = await db.query("Notes");
+    List<Note> list = res.isNotEmpty ? res.map((c) => Note.fromMap(c)).toList() : [];
+    return list;
+  }
+
+  addNote(Note note) async {
+    final db = await database;
+    var res = await db.insert("Notes", note.toMap());
+    return res;
+  }
+
+  deleteNote(int id) async {
+    final db = await database;
+    db.delete("Notes", where: "id = ?", whereArgs: [id]);
+  }
+
+  updateNote(Note note) async {
+    final db = await database;
+    await db.update("Notes", note.toMap(), where: "id = ?", whereArgs: [note.id]);
+  }
+}
+
