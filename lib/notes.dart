75e5596195460d2ce62e62abe4e1379fae9489bb
yyoannle <yoann.letacq1@gmail.com>
2024-04-17 17:10:00 +0200

    update notes include date field

--- lib/notes.dart
+++ lib/notes.dart
@@ -2,11 +2,13 @@ class Note {
   int id;
   String title;
   String body;
+  DateTime date;
 
   Note({
     required this.id,
     required this.title,
     required this.body,
+    required this.date,
   });
 
   Map<String, dynamic> toMap() {
@@ -14,6 +16,7 @@ class Note {
       'id': id,
       'title': title,
       'body': body,
+      'date': date.toIso8601String(),  // Convertit DateTime en String ISO-8601
     };
   }
 
@@ -22,6 +25,7 @@ class Note {
       id: map['id'],
       title: map['title'],
       body: map['body'],
+      date: DateTime.parse(map['date']),  // Convertit une String ISO-8601 en DateTime
     );
   }
 }
